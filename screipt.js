89abd13166c2e8cf6057300d6cea15d8167afb64
Enzo-Moco-pixel <enzo.henriquemocodos.santos@escola.pr.gov.br>
2026-08-31 17:11:15 -0300

    geral

--- screipt.js
+++ screipt.js
@@ -1,17 +1,28 @@
+// Botão
 const botoes = document.querySelectorAll("button");
 
-botoes.forEach(function(botao){
+botoes.forEach(function(botao) {
+
     let curtiu = false;
+
+// Botão é clicado
     botao.addEventListener("click", botaoClicado);
-    function botaoClicado(){
-        console.log("fui clicado");
+
+    function botaoClicado() {
+
+        console.log("Fui clicado!");
+
         let texto = botao.querySelector("span");
-        if(curtiu === false){
-        texto.textContent++;
-        curtiu = true;
-        } else{
+
+        if (curtiu === false) {
+
+            texto.textContent++;
+            curtiu = true;
+
+        } else {
+
             texto.textContent--;
             curtiu = false;
         }
     }
-})
+});
